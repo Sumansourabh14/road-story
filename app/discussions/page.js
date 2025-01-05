fce3670c4afeac535a73faaf7a9518909e4894d7
Suman Sourabh <sumsourabh14@gmail.com>
2025-01-05 21:47:01 +0530

    added create discussion link button

--- app/discussions/page.js
+++ app/discussions/page.js
@@ -1,12 +1,21 @@
 import CenteredPage from "@/components/customUi/layoutSections/CenteredPage";
 import Discussions from "@/components/displayData/Discussions";
 import PageTitle from "@/components/text/PageTitle";
+import Link from "next/link";
 
 const DiscussionsPage = () => {
   return (
     <CenteredPage>
       <section className="text-center mb-16">
         <PageTitle title="Discussions" />
+        <div className="mt-8">
+          <Link
+            href={"/discussions/create"}
+            className="px-20 py-4 bg-black text-white text-xl hover:bg-gray-700 rounded-lg font-bold"
+          >
+            Create Discussion
+          </Link>
+        </div>
       </section>
       <Discussions />
     </CenteredPage>
